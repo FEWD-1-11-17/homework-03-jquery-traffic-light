dbf458430a1117ecb27bb861916c5acf72ce2417
rfrieds <rachels.friedman@gmail.com>
2017-02-12 11:13:35 -0500

    Added Clear Lights Function

--- js/main.js
+++ js/main.js
@@ -4,19 +4,24 @@ $(document).ready(function(){
   $("#slowButton").click(illuminateYellow);
   $("#goButton").click(illuminateGreen);
 
-  function illuminateRed()  {
-    // clearlights();
+  function illuminateRed() {
+    clearlights();
     $('#stopLight').css("background-color","red");
   }
 
-  function illuminateYellow()  {
-    // clearlights();
+  function illuminateYellow() {
+    clearlights();
     $('#slowLight').css("background-color","yellow");    
   }
 
-  function illuminateGreen()  {
-     // clearlights();
+  function illuminateGreen() {
+    clearlights();
     $('#goLight').css("background-color","green");   
   }
 
+  function clearLights() {
+    $('#stopLight').css("background-color","black");
+    $('#slowLight').css("background-color","black"); 
+    $('#goLight').css("background-color","black");
+  }  
 });
