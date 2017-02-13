396dc40cc908e9e11a8b016ab6d86bca92663512
jacobvecchio <JakeVecchio@Jacobs-MacBook-Pro.local>
2017-02-12 19:58:18 -0500

    index.js updated

--- js/index.js
+++ js/index.js
@@ -5,16 +5,23 @@ $(document).ready(function(){
   $("#goButton").click(switchGreen);
 
     function switchRed()  {
+       clearLights();
       $('#stopLight').css("background-color","red");
     }
  
     function switchYellow()  {
+       clearLights();
       $('#slowLight').css("background-color","yellow");    
     }
 
     function switchGreen()  {
+       clearLights();
       $('#goLight').css("background-color","green");   
     }
 
-  
+    function clearLights() {
+      $('#stopLight').css("background-color","black");
+      $('#slowLight').css("background-color","black");
+      $('#goLight').css("background-color","black");
+    }
 });
