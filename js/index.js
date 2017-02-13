ab49dfd2ffb86a78e86e560e0317ec6b45ac394a
jacobvecchio <JakeVecchio@Jacobs-MacBook-Pro.local>
2017-02-13 18:16:29 -0500

    index.js minor changes

--- js/index.js
+++ js/index.js
@@ -5,23 +5,23 @@ $(document).ready(function(){
   $("#goButton").click(switchGreen);
 
     function switchRed()  {
-       clearLights();
-      $('#stopLight').css("background-color","#ff0000");
+       clearLights() ;
+      $("#stopLight").css("background-color","#ff0000");
     }
  
     function switchYellow()  {
-       clearLights();
-      $('#slowLight').css("background-color","#ffff00");    
+       clearLights() ;
+      $("#slowLight").css("background-color","#ffff00");    
     }
 
     function switchGreen()  {
-       clearLights();
-      $('#goLight').css("background-color","#00ff00");   
+       clearLights() ;
+      $("#goLight").css("background-color","#00ff00");   
     }
 
     function clearLights() {
-      $('#stopLight').css("background-color","#000000");
-      $('#slowLight').css("background-color","#000000");
-      $('#goLight').css("background-color","#000000");
+      $("#stopLight").css("background-color","#000000");
+      $("#slowLight").css("background-color","#000000");
+      $("#goLight").css("background-color","#000000");
     }
 });
