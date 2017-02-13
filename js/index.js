846f813b4cb4722369f56222627abb3f63bd668b
Pat Maulion <pmaulion704@gmail.com>
2017-02-13 18:18:00 -0500

    Bundled clearall() in one function
    Grouped ClearAll lights in one

--- js/index.js
+++ js/index.js
@@ -5,23 +5,23 @@ $(document).ready(function() {
     $("#goButton").click(illuminateGreen);
 
     function illuminateRed() {
-        $("#stopLight.bulb").css("backgroundColor", "black");
-        $("#slowLight.bulb").css("backgroundColor", "black");
-        $("#goLight.bulb").css("backgroundColor", "black");
+        clearall();
         $("#stopLight.bulb").css("backgroundColor", "red");
     }
 
     function illuminateYellow() {
-        $("#stopLight.bulb").css("backgroundColor", "black");
-        $("#slowLight.bulb").css("backgroundColor", "black");
-        $("#goLight.bulb").css("backgroundColor", "black");
+        clearall();
         $("#slowLight.bulb").css("backgroundColor", "yellow");    
     }
 
     function illuminateGreen() {
+        clearall();
+        $("#goLight.bulb").css("backgroundColor", "green");   
+    }   
+
+    function clearall() {
         $("#stopLight.bulb").css("backgroundColor", "black");
         $("#slowLight.bulb").css("backgroundColor", "black");
         $("#goLight.bulb").css("backgroundColor", "black");
-        $("#goLight.bulb").css("backgroundColor", "green");   
-    }   
+    }
 });
