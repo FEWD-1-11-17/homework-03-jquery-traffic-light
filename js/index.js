c1c4ca1b1b4795f037af4e5bdb1c61a1b094a0ba
jacobvecchio <JakeVecchio@Jacobs-MacBook-Pro.local>
2017-02-13 17:02:06 -0500

    index.js minor update to hex

--- js/index.js
+++ js/index.js
@@ -6,22 +6,22 @@ $(document).ready(function(){
 
     function switchRed()  {
        clearLights();
-      $('#stopLight').css("background-color","red");
+      $('#stopLight').css("background-color","#ff0000");
     }
  
     function switchYellow()  {
        clearLights();
-      $('#slowLight').css("background-color","yellow");    
+      $('#slowLight').css("background-color","#ffff00");    
     }
 
     function switchGreen()  {
        clearLights();
-      $('#goLight').css("background-color","green");   
+      $('#goLight').css("background-color","#00ff00");   
     }
 
     function clearLights() {
-      $('#stopLight').css("background-color","black");
-      $('#slowLight').css("background-color","black");
-      $('#goLight').css("background-color","black");
+      $('#stopLight').css("background-color","#000000");
+      $('#slowLight').css("background-color","#000000");
+      $('#goLight').css("background-color","#000000");
     }
 });
